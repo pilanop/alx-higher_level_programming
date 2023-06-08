b2a55a0a0494ca89609e75c2382444ab0e0af4bc
Khalid Mohammed <khalarasta104@gmail.com>
2023-06-09 02:46:51 +0300

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-import add_0
+from add_0 import add
 
 a = 1
 b = 2
 
-print("{}".format(a), "+", "{}".format(b), "=", "{}".format(add_0.add(a, b)))
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
