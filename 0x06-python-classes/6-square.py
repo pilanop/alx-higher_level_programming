35e517e785800349da4261c4178e60935707616a
Khalid Mohammed <khalarasta104@gmail.com>
2023-06-28 00:57:45 +0300

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -20,6 +30,7 @@ class Square:
 
     @property
     def position(self):
+        """Get/set the current position of the square."""
         return (self.__position)
 
     @position.setter
@@ -32,9 +43,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
