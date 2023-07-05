278631ec993522e93b0bbc4c6b28444b4e1da2ad
Khalid Mohammed <khalarasta104@gmail.com>
2023-07-05 19:17:32 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    highest = 0
-    highest_name = ''
-
+    my_list = []
     if a_dictionary is None:
         return None
 
     for i in a_dictionary.items():
         pair = list(i)
-        if pair[1] > highest:
-            highest = pair[1]
-            highest_name = pair[0]
+        my_list.append(tuple(pair))
 
-    return highest_name
+    people = my_list
+
+    highest = max(people, key=lambda x: x[1])
+    return highest[0]
