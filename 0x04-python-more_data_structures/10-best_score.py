9f91f58512b1bed3e9a5049caf58a509f390d1ca
Khalid Mohammed <khalarasta104@gmail.com>
2023-07-05 18:54:50 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return (None)
+    highest = 0
+    highest_name = ''
 
-    return (max(a_dictionary, key=a_dictionary.get))
+    if a_dictionary is None:
+        return None
+
+    for i in a_dictionary.items():
+        pair = list(i)
+        if pair[1] > highest:
+            highest = pair[1]
+            highest_name = pair[0]
+
+    return highest_name
