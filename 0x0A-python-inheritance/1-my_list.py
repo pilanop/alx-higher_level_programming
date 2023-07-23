c97ba2d63a7a7873ea15180b44e1d118f1028c33
Khalid Mohammed <khalarasta104@gmail.com>
2023-07-23 04:06:58 +0300

    MyList

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,20 +8,7 @@ and print the elements of the list in ascending order.
 
 class MyList(list):
     """A class representing a custom list.
-
-    This class extends the built-in list class to provide
-    additional functionality.
-
-    Methods:
-        __init__(): Creates a new instance of MyList.
-        print_sorted(): Sorts the elements of the list in
-        ascending order and prints them.
     """
-    def __init__(self):
-        """
-        Creates a new instance of MyList.
-        """
-        super().__init__()
 
     def print_sorted(self):
         """
