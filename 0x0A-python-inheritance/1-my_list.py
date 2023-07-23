0d2d4b707281d714ca46b147a875b158e783ba7c
Khalid Mohammed <khalarasta104@gmail.com>
2023-07-23 04:09:52 +0300

    MyList

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-inherits from the built-in list class and adds a method print_sorted
+"""inherits from the built-in list class and adds a method print_sorted
 """
 
 
@@ -8,7 +7,6 @@ class MyList(list):
     """A class representing a custom list.
     """
     def print_sorted(self):
-        """
-        Sorts the elements of the list and prints them.
+        """Sorts the elements of the list and prints them.
         """
         print(sorted(self))
