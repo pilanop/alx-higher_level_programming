834a21e035d34ac7b27afc406d1b5e04b5f6e05a
Khalid Mohammed <khalarasta104@gmail.com>
2023-07-25 16:54:59 +0300

    5. Save Object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""
+Defines a function that writes an Object to a text file, using a JSON
+"""
+import json
 
-filename = "my_list.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
 
-filename = "my_dict.json"
-my_dict = {
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-save_to_json_file(my_dict, filename)
+def save_to_json_file(my_obj, filename):
+    """
+    Saves the given object to a JSON file.
 
-try:
-    filename = "my_set.json"
-    my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    Args:
+        my_obj (object): The object to be saved.
+        filename (str): The name of the file to save the object to.
+
+    Returns:
+        None
+    """
+    with open(filename, mode='w', encoding='utf8') as f:
+        f.write(json.dumps(my_obj))
