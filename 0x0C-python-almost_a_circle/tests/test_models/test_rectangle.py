88ed03e67737bdfcaca30bb1ebe136a6ed1e204c
Khalid Mohammed <khalarasta104@gmail.com>
2023-08-01 16:30:58 +0300

    3. Validate attributes (test)

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -39,5 +39,36 @@ class TestRectangle(unittest.TestCase):
         pass
 
 
+class TestRectangle_Validate_Attributes(unittest.TestCase):
+    """
+    3. Validate attributes
+    This class contains unit tests for validating the attributes of the
+    Rectangle class.
+    """
+    def test_invalid_width(self):
+        with self.assertRaises(TypeError):
+            Rectangle("not a number", 5)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 5)
+
+    def test_invalid_height(self):
+        with self.assertRaises(TypeError):
+            Rectangle(5, "not a number")
+        with self.assertRaises(ValueError):
+            Rectangle(5, 0)
+
+    def test_invalid_x(self):
+        with self.assertRaises(TypeError):
+            Rectangle(5, 6, "not a number")
+        with self.assertRaises(ValueError):
+            Rectangle(5, 6, -1)
+
+    def test_invalid_y(self):
+        with self.assertRaises(TypeError):
+            Rectangle(5, 6, 0, "not a number")
+        with self.assertRaises(ValueError):
+            Rectangle(5, 6, 0, -1)
+
+
 if __name__ == "__main__":
     unittest.main()
