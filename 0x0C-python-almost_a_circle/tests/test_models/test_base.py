81d66874413ed6df8da07a3b18ad98c646926f70
Khalid Mohammed <khalarasta104@gmail.com>
2023-08-03 07:49:43 +0300

    16. JSON string to file (test)

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,9 +2,12 @@
 """
 Defines unittests for models/base.py.
 """
+import os
 import json
 import unittest
 from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -46,6 +49,38 @@ class TestBase_to_json_string(unittest.TestCase):
     def test_None_list(self):
         # Test with None
         self.assertEqual(Base.to_json_string(None), "[]")
+
+
+class TestBase_save_to_file(unittest.TestCase):
+    """
+    16. JSON string to file
+    This class is a unit test for the `Base` class 'save_to_file' class method.
+    """
+
+    def setUp(self):
+        self.r1 = Rectangle(10, 7, 2, 8)
+        self.r2 = Rectangle(2, 4)
+
+    def test_save_to_file(self):
+        Base.save_to_file([self.r1, self.r1])
+
+        # Verify the file contents for Rectangle
+        if os.path.isfile("Rectangle.json"):
+            with open("Rectangle.json", "r") as f:
+                content = f.read()
+                expected = ('[{"id": 1, "width": 10, "height": 7, "x": 2, '
+                            '"y": 8}, {"id": 2, "width": 2, "height": 4, '
+                            '"x": 0, "y": 0}]')
+                self.assertEqual(json.loads(content), json.loads(expected))
+
+    def tearDown(self):
+        """
+        Delete generated files after each test
+        """
+        if os.path.isfile("Rectangle.json"):
+            os.remove("Rectangle.json")
+        if os.path.isfile("Square.json"):
+            os.remove("Square.json")
 
 
 if __name__ == '__main__':
