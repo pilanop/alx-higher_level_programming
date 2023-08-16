cbe54575230a11b4f7609d3a9cf13c0e22c12ae7
Khalid Mohammed <khalarasta104@gmail.com>
2023-08-16 03:25:31 +0300

    11. Select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- This document retrieves the 'score' and 'name' from 'second_table' where the score is greater or equal to 10, and orders the results by 'score' in descending order.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
