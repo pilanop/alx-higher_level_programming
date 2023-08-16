bb9c0187c50613f51e4c7926c72d676413236f4e
Khalid Mohammed <khalarasta104@gmail.com>
2023-08-16 03:14:01 +0300

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS second_table (
 INSERT INTO second_table VALUES (1, 'John', 10);
 INSERT INTO second_table VALUES (2, 'Alex', 3);
 INSERT INTO second_table VALUES (3, 'Bob', 14);
-INSERT INTO second_table VALUES (4, 'Bob', 8);
+INSERT INTO second_table VALUES (4, 'George', 8);
