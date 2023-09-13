d2bc7e8b4944988337741c0a3cca4d813a86c2c8
Khalid Mohammed <khalarasta104@gmail.com>
2023-09-13 03:56:52 +0300

    3. Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const arg = process.argv;
 if (arg[2] === undefined) {
-    console.log('No argument')
-}
-else {console.log(arg[2]);}
+  console.log('No argument');
+} else { console.log(arg[2]); }
