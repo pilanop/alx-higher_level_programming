f7ba71cecb5caa36f57fc5c95a825d7419530670
Khalid Mohammed <khalarasta104@gmail.com>
2023-09-14 15:32:51 +0300

    10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/Users/khalidmohammed/.nvm/versions/node/v18.17.0/bin/node
+const arg = process.argv;
+
+function factorial (n) {
+  if (isNaN(n) || n === 1 || n === 0) {
+    return 1;
+  }
+  return parseInt(n) * (factorial(parseInt(n) - 1));
+}
+
+console.log(factorial(arg[2]));
