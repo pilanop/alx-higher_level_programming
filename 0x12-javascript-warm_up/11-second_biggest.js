850ed4368b8e21d5a08974a07c12cdadb2b3d259
Khalid Mohammed <khalarasta104@gmail.com>
2023-09-14 22:04:31 +0300

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ for (let i = 2; i < arg.length; i++) {
   newArg.push(parseInt(arg[i]));
 }
 newArg.sort();
-if (arg[2] === undefined || newArg.length === 1) {
+if (arg.length <= 3) {
   console.log(0);
 } else {
   console.log(newArg[newArg.length - 2]);
