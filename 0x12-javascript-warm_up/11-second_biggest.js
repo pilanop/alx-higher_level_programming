d0d51313046958ee8d7a88f886c7244905c90497
Khalid Mohammed <khalarasta104@gmail.com>
2023-09-14 22:00:56 +0300

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
+#!/Users/khalidmohammed/.nvm/versions/node/v18.17.0/bin/node
 const arg = process.argv;
 const newArg = [];
 for (let i = 2; i < arg.length; i++) {
   newArg.push(parseInt(arg[i]));
 }
 newArg.sort();
-if (arg[2] === undefined || arg.length === 3) {
+if (arg[2] === undefined || newArg.length === 1) {
   console.log(0);
 } else {
   console.log(newArg[newArg.length - 2]);
