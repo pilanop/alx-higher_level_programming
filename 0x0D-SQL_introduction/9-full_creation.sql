e972f8300c704bd8d32e8ec35be944c473c37215
Khalid Mohammed <khalarasta104@gmail.com>
2024-01-13 18:17:45 +0300

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,12 @@
 -- This script creates a table named 'second_table' if it doesn't already exist, then inserts four records into it.
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 
+INSERT INTO second_table (id, name, score)
+VALUES (id = 1, name = 'John', score = 10),
+       (id = 2, name = 'Alex', score = 3),
+       (id = 3, name = 'Bob', score = 14),
+       (id = 4, name = 'George', score = 18)
